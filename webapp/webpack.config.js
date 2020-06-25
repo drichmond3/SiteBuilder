d4f856a3e494fb2a27b6ad45a883df5389463a18
Darrien <darrien.richmond@gmail.com>
2020-06-24 23:54:39 -0500

    project setup [npm, webpack, react, babel, eslint]

---
+++ webapp/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [new HtmlWebpackPlugin()],
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', "@babel/preset-react"]
+                    }
+                }
+            }
+        ]
+    }
+};
