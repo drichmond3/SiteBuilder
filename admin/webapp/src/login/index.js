e5535f2c7f6c5453f2e53ca23c446fedf37e6be5
Darrien <darrien.richmond@gmail.com>
2020-06-28 17:17:55 -0500

    add css loaders, linter, and login and main page skeletons

---
+++ admin/webapp/src/login/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app.js';
+
+const root = (() => {
+  const element = document.createElement('div');
+  element.id = 'root';
+  return element;
+})();
+
+document.body.appendChild(root);
+ReactDOM.render(<App />, root);
--- admin/webapp/webpack.config.js
+++ admin/webapp/webpack.config.js
@@ -1,25 +1,50 @@
+/* eslint-disable */
+const props = require('./properties.json');
 const path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [new HtmlWebpackPlugin()],
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', "@babel/preset-react"]
-                    }
-                }
-            }
-        ]
-    }
+  entry: { login: './src/login/index.js', main: './src/main/index.js' },
+  output: {
+    filename: '[name].[contentHash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ['login'],
+      filename: 'login/index.html',
+    }),
+    new HtmlWebpackPlugin({
+      chunks: ['main'],
+      filename: 'main/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: props.environments.local.webappPort,
+    open: true,
+    openPage: 'login',
+    historyApiFallback: true,
+  }
 };
+if (process.env.NODE_ENV == 'development') {
+  module.exports.devtool = 'eval-source-map';
+}
