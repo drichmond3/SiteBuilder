c39df34ae8bb93366055c467ed744ec89849180e
Marisa Richmond <marisa.richmond94@gmail.com>
2020-06-30 20:11:59 -0500

    Submit -> Login

--- admin/webapp/src/login/app.js
+++ admin/webapp/src/login/app.js
@@ -64,7 +64,7 @@ export default function App() {
               onClick={handleSubmit}
               type='submit'
             >
-              Submit
+              Login
             </Button>
           </Col>
         </Row>
